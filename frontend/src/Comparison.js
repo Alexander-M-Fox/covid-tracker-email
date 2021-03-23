1995cfcfa59d53112665db1a9807b904e1e421e9
Alexander-M-Fox <alexfox9973@gmail.com>
2021-03-23 22:04:09 +0000

    fixed proxy bug (missing trailing '/'), working calls for country data

--- frontend/src/Comparison.js
+++ frontend/src/Comparison.js
@@ -1,17 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 const axios = require("axios");
 
 function Comparison(props) {
     axios.post("/api/covid/countries", props.compareList).then((res) => {
         console.table("response of /api/covid/countries post request", res);
+        covidData = res.body;
     });
 
     return (
         <div>
             <h1>comparison page</h1>
-            {props.compareList.map((country, index) => {
-                return <p key={index}>{country.name}</p>;
-            })}
         </div>
     );
 }
@@ -20,5 +18,5 @@ export default Comparison;
 
 // Comparison requirements
 // 1. send compareList to backend requesting for covid data (to /api/covid/countries)
-// 2. display result nicely (opt in graphs)
+// 2. display result nicely (opt in graphs) TODO: <-- next
 // 3. have "send daily updates via email" button send compareList to /api/covid/emailMe route
