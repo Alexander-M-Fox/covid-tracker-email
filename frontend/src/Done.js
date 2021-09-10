099a5c2aadf4b0e4ed163988d7cf8d6dc0a885a9
Alexander-M-Fox <alexfox9973@gmail.com>
2021-09-10 17:45:41 +0100

    start done page

---
+++ frontend/src/Done.js
@@ -0,0 +1,31 @@
+import { React, useEffect } from "react";
+import { BrowserRouter as Router, useHistory } from "react-router-dom";
+const axios = require("axios");
+
+function Done() {
+    let history = useHistory();
+
+    useEffect(() => {
+        axios.get("/api/checkAuth").then((res) => {
+            if (res.data !== true) {
+                history.push("/login");
+            }
+        });
+    }, []);
+    return (
+        <div className="containerMedium">
+            <h1>you're all set up!</h1>
+            <div className="containerShort">
+                <div>
+                    <div className="choiceButtons">
+                        <button onClick={() => {}}>logout</button>
+                        <p></p>
+                        <button onClick={() => {}}>change settings</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Done;
